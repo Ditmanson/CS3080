67929cff127f2eda9a9e46f7dba99cfafbc1b8f6
Ditmanson <97914433+Ditmanson@users.noreply.github.com>
2023-01-16 19:14:14 -0700

    a

--- test.py
+++ test.py
@@ -5,7 +5,8 @@ myName = input()
 print("it is good to meet you, " + myName)
 print("The length of your name is:")
 print(len(myName))
-
+# %%
 print("What is your age?")  # ask for a age
 myAge = input()
 print("You will be " + str(int(myAge)+1) + " in a year.")
+# %%
