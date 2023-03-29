f431399954dc1f617f79365346c4f3b1c073fe81
Travis Ditmanson <travisditmanson@gmail.com>
2023-03-29 10:00:44 -0600

    didn't do anything

--- matplot_exercises/matplotlib_ex_2.py
+++ matplot_exercises/matplotlib_ex_2.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-csv_file = pd.read_csv('company_sales_data.csv')
+csv_file = pd.read_csv('~/Documents/CS3080/matplot_exercises/company_sales_data.csv')
 profitList = csv_file ['total_profit'].tolist()
 monthList= csv_file['month_number'].tolist()
 plt.xlabel('Month number')
@@ -10,8 +10,7 @@ plt.ylabel('Profit in dollar')
 plt.xticks(monthList)
 plt.title('Company profit per month')
 plt.yticks([100000,200000,300000,400000,500000])
-plt.plot(monthList, profitList, label='MonthWise profit data of last year',
-        color ='r', marker='o', markerfacecolor='k',linestyle='--', linewidth=3)
-#print(csv_file)
+plt.plot(monthList, profitList, label='MonthWise profit data of last year', color ='r', marker='o', markerfacecolor='k',linestyle='--', linewidth=3)
+print(csv_file)
 plt.show()
 
